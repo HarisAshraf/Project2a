0b0ced19fab1fbd1232394b702b147715f5916d2
HarisAshraf <harisashraf@aol.com>
2023-01-05 08:10:56 -0800

    added teleop.launch

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/workspace/Project4/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/workspace/Project4/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/workspace/Project4/catkin_ws/devel;/opt/ros/melodic' != '' else []
